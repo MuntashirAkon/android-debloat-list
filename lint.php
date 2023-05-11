ccd1252117451d4417093eaaca81988828103191
Muntashir Al-Islam <muntashirakon@riseup.net>
2023-05-11 11:20:22 +0600

    Update Google descriptions

---
+++ lint.php
@@ -0,0 +1,13 @@
+<?php
+
+$json_files = array_filter(scandir(__DIR__), function ($item) {
+    return str_ends_with($item, ".json");
+});
+
+foreach ($json_files as $json_file) {
+    $list = json_decode(file_get_contents(__DIR__ . '/' . $json_file), true);
+    usort($list, function ($o1, $o2) {
+        return $o1['id'] <=> $o2['id'];
+    });
+    file_put_contents(__DIR__ . '/' . $json_file, json_encode($list, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+}
